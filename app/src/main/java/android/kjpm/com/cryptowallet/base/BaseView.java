2bdb37c958171ecc1489fbccb8e7e2f512bdbbf8
Albert Cueme <shortcut7777@gmail.com>
2017-07-08 02:56:17 +0800

    Add BaseView

---
+++ app/src/main/java/android/kjpm/com/cryptowallet/base/BaseView.java
@@ -0,0 +1,10 @@
+package android.kjpm.com.cryptowallet.base;
+
+/**
+ * Created by shortcut on 7/8/2017.
+ */
+
+public interface BaseView<T> {
+    void setPresenter(T presenter);
+    void showToast(String message);
+}
